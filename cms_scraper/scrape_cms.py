1000812de1922d6227ccdc239137c3ab8644b50a
veena.kalburgi <veena.kalburgi@gmail.com>
2020-09-12 18:31:51 -0500

    web-scrapper with BeautifulSoup is now complete and working

--- cms_scraper/scrape_cms.py
+++ cms_scraper/scrape_cms.py
@@ -6,17 +6,17 @@ import csv
 source = requests.get('http://coreyms.com').text
 soup = BeautifulSoup(source, 'html5lib')
 
-csv_file = open('cms_scraper/cms_scrape.csv', 'w')
+csv_file = open('cms_scrape.csv', 'w', newline='')
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['headline', 'summary', 'video_link'])
 
 for article in soup.find_all('article'):
+
     headline = article.h2.a.text
-    print(headline)
-
     summary = article.find('div', class_='entry-content').p.text
-    print(summary)
 
+    # The videos in the website are embedded videos. They are not the actual link to teh videos
+    # we will have to extract the actual link manually. See Corey (27:30) onwards
     try:
         vid_src = article.find('iframe', class_='youtube-player')['src']
         vid_id = vid_src.split('/')[4]
@@ -24,9 +24,7 @@ for article in soup.find_all('article'):
         yt_link = f'https://youtube.com/watch?v={vid_id}'
     except Exception as e:
         yt_link = None
+    csv_writer.writerow([headline, summary, yt_link])
 
-    print(yt_link)
-    print()
-
-csv_writer.writerow([headline, summary, yt_link])
 csv_file.close()
+
